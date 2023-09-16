673d399cc71aa9f81fa1596aeaf498c625f26d25
unitedsabre <biryuchev98@mail.ru>
2023-09-16 17:57:49 +0500

    lr1

--- src/main/java/ru/biryuchev/MyFirstTestAppStringBoot/MyFirstTestAppStringBootApplication.java
+++ src/main/java/ru/biryuchev/MyFirstTestAppStringBoot/MyFirstTestAppStringBootApplication.java
@@ -3,8 +3,6 @@ package ru.biryuchev.MyFirstTestAppStringBoot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.ArrayList;
-
 @SpringBootApplication
 public class MyFirstTestAppStringBootApplication {
 
